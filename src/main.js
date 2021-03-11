dc86c5490961d0bafeebde95a10d20d0bb83a162
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2021-03-11 14:01:08 -0300

    :sparkles: add user profile page for mobile

--- src/main.js
+++ src/main.js
@@ -4,6 +4,27 @@ import App from './App.vue'
 import router from './router'
 import './assets/global.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faArrowLeft,
+  faFolderOpen,
+  faHome,
+  faLaptop,
+  faMale,
+  faUser,
+  faUsers
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faArrowLeft)
+library.add(faFolderOpen)
+library.add(faHome)
+library.add(faLaptop)
+library.add(faMale)
+library.add(faUser)
+library.add(faUsers)
+
 createApp(<App />)
   .use(router)
+  .component('FontAwesomeIcon', FontAwesomeIcon)
   .mount('#app')
