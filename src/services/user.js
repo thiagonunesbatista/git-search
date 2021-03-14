b189533595f3a9237519f8098d0e4948d9820d08
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2021-03-14 12:34:14 -0300

    :sparkles: add users list for mobile

--- src/services/user.js
+++ src/services/user.js
@@ -5,3 +5,11 @@ export const getUser = username =>
 
 export const getUserRepos = (username, perPage = 8) =>
   axios(`https://api.github.com/users/${username}/repos?per_page=${perPage}`)
+
+export const searchUsers = (searchParam, perPage = 10) =>
+  axios(
+    `https://api.github.com/search/users?q=${searchParam}&per_page=${perPage}`
+  )
+
+export const showUsers = (perPage = 10) =>
+  axios(`https://api.github.com/users?per_page=${perPage}`)
---
+++ src/views/UsersList/index.vue
@@ -0,0 +1,154 @@
+<template>
+  <Navbar title="Lista de Usuários" />
+  <Loader v-if="loading" />
+
+  <p v-if="error" class="error-message">{{ error }}</p>
+
+  <div v-if="!error">
+    <div class="input-wrapper">
+      <button
+        @click="handleSearch"
+        :disabled="searchInput.length === 0"
+        class="search-button"
+      >
+        <FontAwesomeIcon icon="search" class="search-icon" />
+      </button>
+      <input v-model="searchInput" type="text" class="search-input" />
+    </div>
+
+    <div class="users-wrapper">
+      <Link
+        v-for="user in users"
+        :key="user.id"
+        :to="{
+          name: 'UserProfile',
+          params: {
+            username: user.login
+          }
+        }"
+        class="user"
+      >
+        <img :src="user.avatar_url" alt="user.login" />
+        <p class="username">{{ user.login }}</p>
+      </Link>
+
+      <button
+        v-if="!finishedUsers && nextLink"
+        @click="loadMoreUsers(nextLink === lastLink)"
+        class="load-more"
+      >
+        Carregar mais
+      </button>
+
+      <button v-else class="load-more disabled">
+        Não há mais repositórios
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import Loader from '@/components/Loader'
+import Navbar from '@/components/Navbar'
+
+import { parseURLPagination } from '@/helpers/github'
+
+import { searchUsers, showUsers } from '@/services/user'
+
+export default {
+  name: 'UsersList',
+  components: {
+    Loader,
+    Navbar
+  },
+  data() {
+    return {
+      error: '',
+      finishedUsers: false,
+      lastLink: '',
+      loading: true,
+      nextLink: '',
+      searchInput: '',
+      users: []
+    }
+  },
+  mounted() {
+    this.loadData()
+  },
+  methods: {
+    async loadData() {
+      try {
+        const searchParam = this.$route.params.search
+
+        this.loading = true
+        this.searchInput = searchParam || ''
+
+        const response = searchParam
+          ? await searchUsers(searchParam)
+          : await showUsers()
+
+        this.users = searchParam ? response.data.items : response.data
+
+        this.nextLink =
+          response.headers?.link &&
+          parseURLPagination(response.headers.link, 'next')
+
+        this.lastLink =
+          searchParam && parseURLPagination(response.headers.link, 'last')
+      } catch (error) {
+        this.error = 'Erro interno. Entre em contato com o desenvolvedor!'
+      } finally {
+        this.loading = false
+      }
+    },
+    async handleSearch() {
+      try {
+        this.loading = true
+
+        const response = await searchUsers(this.searchInput)
+
+        this.users = response.data.items
+
+        this.nextLink =
+          response.headers?.link &&
+          parseURLPagination(response.headers.link, 'next')
+
+        this.lastLink =
+          response.headers?.link &&
+          parseURLPagination(response.headers.link, 'last')
+      } catch (error) {
+        this.error = 'Erro interno. Entre em contato com o desenvolvedor!'
+      } finally {
+        this.loading = false
+      }
+    },
+    async loadMoreUsers(areLastUsers) {
+      try {
+        this.loading = true
+
+        const response = await axios(this.nextLink)
+
+        this.finishedUsers = areLastUsers
+
+        const isSearch = Boolean(this.searchInput && this.lastLink)
+
+        this.users = isSearch
+          ? [...this.users, ...response.data.items]
+          : [...this.users, ...response.data]
+
+        this.nextLink =
+          response.headers?.link &&
+          parseURLPagination(response.headers.link, 'next')
+      } catch (error) {
+        this.error = 'Erro interno. Entre em contato com o desenvolvedor!'
+      } finally {
+        this.loading = false
+      }
+    }
+  }
+}
+</script>
+
+<style src="./styles.sass" lang="sass" scoped />
